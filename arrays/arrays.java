c9faac660a2864719164ba840215cc008113371c
prapti-gupta-1805 <prapti18056@gmail.com>
2024-11-22 19:32:46 +0530

    arrays.java created

---
+++ arrays/arrays.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class arrays {
+    public static void main(String[] args) {
+        // creation
+        int arr[] = new int[1];
+        String chars[] = {"a", "b", "c"};
+        
+        // input
+        Scanner sc = new Scanner(System.in);
+        int marks[] = new int[3];
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter marks: ");
+            marks[i] = sc.nextInt();
+        }
+    }
+}
