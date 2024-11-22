30abd052ae6652320bc7969199212db48fd6d740
prapti-gupta-1805 <prapti18056@gmail.com>
2024-11-22 23:24:37 +0530

    Update arrays.java

--- arrays/arrays.java
+++ arrays/arrays.java
@@ -1,20 +1,57 @@
 import java.util.*;
 
 public class arrays {
+    // Linear search method
+    public static void linearSearch(int numbers[], int key) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+                System.out.println("Key found at index: " + i);
+                return;
+            }
+        }
+        System.out.println("Key not found");
+    }
+
+    //smallest and largest number
+    public static void smallest_largest(int numbers[]) {
+        int largest= Integer.MIN_VALUE; //Integer.MIN_VALUE = -infinity
+        int smallest= Integer.MAX_VALUE; //+infinity
+        for (int i=0; i<numbers.length; i++) {
+            if (numbers[i]>largest) {
+                largest=numbers[i];
+            }
+            if (numbers[i]<smallest) {
+                smallest=numbers[i];
+            }
+        }
+        System.out.println("Largest number: " + largest);
+        System.out.println("Smallest number: " + smallest);
+    }
+
+    //Binary Search
+    //prerequisite - sorted array
+
     public static void main(String[] args) {
-        // creation
-        int arr[] = new int[1];
-        String chars[] = {"a", "b", "c"};
+        int numbers[]= {1,2,3,4,5};
+        // Array creation
+        //int[] arr = new int[1];
+        //String[] chars = {"a", "b", "c"};
         
-        // input
+        // Input
         Scanner sc = new Scanner(System.in);
-        int marks[] = new int[3];
+        int[] marks = new int[3];
         
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < marks.length; i++) {
             System.out.print("Enter marks: ");
             marks[i] = sc.nextInt();
-
-        System.out.print(marks.length); // print length of array
         }
+        
+        System.out.println("Length of the array: " + marks.length); // Print length of array
+
+        // Array passed as parameter in the function
+        System.out.print("Enter the key to search: ");
+        int key = sc.nextInt();
+        linearSearch(marks, key);
+        smallest_largest(marks);
     }
 }
